1353bdf7311f39b091a0ad982ae71d5275640e0b
Emiliya-tayn <75200265+Emiliya-tayn@users.noreply.github.com>
2021-03-23 13:55:11 +0500

    ver9

--- src/components/alert-message/alert-message.js
+++ src/components/alert-message/alert-message.js
@@ -5,35 +5,35 @@ import './alert-message.css';
 
 
 const AlertMessage = ({getAgreeSaveCookies, isCookieSendingAgree}) => {
-
-    let className = 'alert';
+    
 
     if(isCookieSendingAgree) {
-        className += ' disable';
+        return null;
     }
     return (
-        <Alert className={className}
-            message="Accept Cookies?"
-            description="This website uses cookies in order to offer you the most relevant information. Please accept cookies for optimal performance."
-            type="info"
-            action={
-                <Space direction="vertical">
-                    <Button size="small" type="primary" onClick={getAgreeSaveCookies}>
-                        Accept
-                    </Button>
-                    <Button size="small" danger type="ghost">
-                        Decline
-                    </Button>
-                </Space>
-            }
-            closable
+        <Alert className='alert'
+               message="Accept Cookies?"
+               description="This website uses cookies in order to offer you the most relevant information. Please accept cookies for optimal performance."
+               type="info"
+               action={
+                   <Space direction="vertical">
+                       <Button size="small" type="primary" onClick={getAgreeSaveCookies}>
+                           Accept
+                       </Button>
+                       <Button size="small" danger type="ghost">
+                           Decline
+                       </Button>
+                   </Space>
+               }
+               closable
         />
     )
+
 }
 
 AlertMessage.defaultProps = {
     getAgreeSaveCookies: ()=>{},
-    isCookieSendingAgree: 'true'
+    isCookieSendingAgree: ''
 }
 AlertMessage.propTypes = {
     getAgreeSaveCookies: PropTypes.func,
